4d68432a1cf327f5768909d9def9ab397a06a95e
Orin Walker <owalker@feldinc.com>
2019-10-28 16:24:18 -0400

    Added angular material and started social media login authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService, FacebookLoginProvider, SocialUser } from 'angularx-social-login';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'My Todo List';
+
+export class AppComponent implements OnInit {
+  title = 'BDA Todos';
+  user: SocialUser;
+  loggedIn: boolean;
+
+  constructor(private authService: AuthService) {
+
+  }
+
+  signInWithFB(): void {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
+
+  signOut(): void {
+    this.authService.signOut();
+  }
+
+  ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+      console.log(this.user);
+    });
+  }
+
 }
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,34 @@ import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
 import { CommonModule } from '@angular/common';
+import { SocialLoginModule, AuthServiceConfig, FacebookLoginProvider } from 'angularx-social-login';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule, MatButtonModule, MatCardModule } from '@angular/material';
+
+
+
+// const config = new AuthServiceConfig([
+//   {
+//     id: FacebookLoginProvider.PROVIDER_ID,
+//     provider: new FacebookLoginProvider('413438242922334')
+//   }
+// ]);
+
+const config = new AuthServiceConfig([
+  // {
+  //   TODO: Add Google Login Provider
+  //   id: GoogleLoginProvider.PROVIDER_ID,
+  //   provider: new GoogleLoginProvider('Your-Google-Client-ID')
+  // },
+  {
+    id: FacebookLoginProvider.PROVIDER_ID,
+    provider: new FacebookLoginProvider('413438242922334')
+  }
+]);
+
+export function provideConfig() {
+  return config;
+}
 
 @NgModule({
   declarations: [
@@ -16,9 +44,19 @@ import { CommonModule } from '@angular/common';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CommonModule
+    CommonModule,
+    // SocialLoginModule,
+    SocialLoginModule.initialize(config),
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule
   ],
-  providers: [],
+  providers: [
+    // AuthServiceConfig,
+    // useFactory: provideConfig
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
